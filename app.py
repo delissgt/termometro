3abe2d0a227d044974f5fce4970db50b356137da
delissgt <delissgt@gmail.com>
2018-09-19 14:14:15 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,210 @@
+#CREACION DE UN TERMOMETRO QUE SUENA CUANDO LLEGA A CIERTA TEMPERATURA QUE INDICA EL USUARIO
+
+from PyQt4.QtGui import (QFileDialog, QMainWindow, QApplication, QComboBox, QLabel, QMessageBox)
+from PyQt4.QtCore import QThread, QTimer, QProcess, pyqtSignal as Signal
+import serial.tools.list_ports
+import sys
+import uitermometro #arch interface creado con QTDesigner
+import serial, time #se necesita para usar arduino
+import re #expresion regular
+import pyqtgraph as pg #para la grafica pintar
+from PyQt4.QtMultimedia import (QAudioDeviceInfo, QAudioOutput)
+
+class MainWindow(QMainWindow, uitermometro.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self) #para jalar todos los componentes de la interfaz
+        #con la linea de arriba se puede ver que jala los componentes, y que to-do salio bien
+
+        miLabel = QLabel("Dispositivos: ")
+        self.miComboBox = QComboBox(self)
+        self.toolBar.addWidget(miLabel)
+        self.toolBar.addWidget(self.miComboBox)
+
+        self.actionRefresh.triggered.connect(self.funcionRefrescar)#TODO trabajando
+
+        self.pushButton_Iniciar.clicked.connect(self.funcionStart)
+
+        #linea para poder mostrar que dispositivos estan conectados TODO
+        dispositivos = serial.tools.list_ports.comports()
+
+        #crear grafica y sus propiedades
+        self.crear_grafica()  # funcion
+        self.grafica_datos_x = []
+        self.grafica_datos_y = []
+        self.contador_x = 0
+
+        for usb in dispositivos:
+            print (usb.device)
+            self.miComboBox.addItem(usb.device) #agrega un elemento al ComboBox
+
+        #llamar
+        self.funcionCurrentUsb()
+
+        #senial para detectar el dispositivo deleccionado
+        self.miComboBox.currentIndexChanged.connect(self.funcionCurrentUsb)
+
+
+
+    def crear_grafica(self):
+        self.pluma = pg.mkPen(width=2, color='y')
+        self.graphicsView.plotItem.showGrid(True, True, 0.5)
+        self.graphicsView.setXRange(0, 59)
+        self.graphicsView.setYRange(0, 100)
+
+
+    def actualizar_grafica(self, temperatura):
+        if len(self.grafica_datos_y) == 60:
+            self.grafica_datos_y.pop(0)
+
+            #extend agrega dato a la lista, mas rapido que apend
+        #self.grafica_datos_y.extend([psutil.virtual_memory().percent])
+        self.grafica_datos_y.extend([temperatura])
+
+        if len(self.grafica_datos_x) == 60:
+            pass
+        else:
+            self.grafica_datos_x.extend([self.contador_x])
+            self.contador_x += 1
+
+        self.graphicsView.plot(self.grafica_datos_x, self.grafica_datos_y[::-1], pen=self.pluma, clear=True)
+
+
+        self.label_temperatura.setText(str(temperatura))
+        if temperatura >= self.spinBox_tempe_max.value():
+            print("comparacion datos")
+
+            if self.checkBox_alarma.isChecked():
+                print("alarma!!!")
+                #sonido = QAudioDeviceInfo.defaultOutputDevice()
+                #alarma = QAudioOutput(sonido)
+                #alarma.start()
+                print('\a')
+
+
+         #QAudioDeviceInfo info(QAudioDeviceInfo.defaultOutputDevice());
+
+
+    def funcionCurrentUsb(self):
+        self.currentusb = self.miComboBox.currentText()
+        print("usb actual: ", self.currentusb)
+        #print(type(self.currentusb))
+
+
+    def funcionStart(self):
+        #Boton funcionando
+        self.contador_x = 0
+        self.grafica_datos_x=[]
+        self.grafica_datos_y =[]
+
+        if self.pushButton_Iniciar.text() == "&Start":
+            print("boton funcionando")
+            self.pushButton_Iniciar.setText("Stop")
+            #limpiar texto de temperatura
+            self.label_temperatura.setText("")
+            #limpiar grafica
+            self.graphicsView.plot(self.grafica_datos_x, self.grafica_datos_y[::-1], pen=self.pluma, clear=True)
+            #obtener texto temperatura maxima
+            valorSpinBox = self.spinBox_tempe_max.value()
+            self.spinBox_tempe_max.setDisabled(True)
+            print("valor spin: ", valorSpinBox)
+            #checkBox
+            self.checkBox_alarma.setDisabled(True)
+
+            try:
+                #Leer arduino
+                self.arduino = serial.Serial(str(self.currentusb), 9600)#abre puerto arduino
+
+                #Hilo lee e imprime lo que recive del arduino, necesario para que no interfiera con interfaz
+                self.mi_hilo = ClaseHilo(self.arduino, parent=self)
+
+                self.mi_hilo.mi_senial_num.connect(self.actualizar_grafica)
+
+                self.mi_hilo.start()
+
+            except serial.serialutil.SerialException:
+                QMessageBox.critical(self, 'Message', 'dispositivo desconectado', QMessageBox.Ok)
+
+
+        #Boton detenido
+        else:
+            try:
+                self.mi_hilo.terminate()
+                self.arduino.close()
+            except AttributeError:
+                print("no se encuentra dispositivo")
+            self.pushButton_Iniciar.setText("&Start")
+            print("boton detenido")
+            self.spinBox_tempe_max.setDisabled(False)
+            self.checkBox_alarma.setDisabled(False)
+
+
+    def funcionRefrescar(self):
+        self.miComboBox.clear()
+        dispositivos = serial.tools.list_ports.comports()
+        for usb in dispositivos:
+            print (usb.device)
+            self.miComboBox.addItem(usb.device) #agrega un elemento al ComboBox
+
+
+
+class ClaseHilo(QThread):
+
+    mi_senial_num = Signal(float)
+
+    def __init__(self, arduino, parent=None):
+        super(ClaseHilo, self).__init__(parent)
+        self.arduino = arduino
+
+
+    def run(self):
+        while True:
+            time.sleep(2)
+            try:
+                rawString = self.arduino.readline()
+            except serial.serialutil.SerialException:
+                print("dispositivo desconectado")
+            else:
+                #comparacion = re.search('A[0-9].[0-9]+A', rawString).group()
+                comparacion = re.search('A[0-9]+\.[0-9]+A', str(rawString))
+
+                if comparacion is not None:
+                    #comparacion.group()
+                    temperatura = comparacion.group()
+                    #print("comparacion:", comparacion.group())
+
+                    #se trata "comparacion" para que solo se mande el numero
+                    #numTemperatura = comparacion[1:]
+                    numTemperatura = temperatura[1:]
+                    #print("numTemperatura: ", numTemperatura)
+
+                    #print("1A", numTemperatura)
+                    longitud = len(numTemperatura)
+                    numTemperatura = numTemperatura[:longitud-1]
+                    print("texto a enviar", numTemperatura)
+
+                    #emito la senial
+                    self.mi_senial_num.emit(float(numTemperatura))
+
+            #self.arduino.close() quitar
+
+
+
+
+
+#cuando comparo con None se utiliza:
+# if variable is None
+# if varible is not None
+# No se utiliza == o !=
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setOrganizationDomain("www.deliss.me")
+    app.setOrganizationName("deliss")
+    app.setApplicationVersion("1")
+    app.setApplicationName("deliss_me")
+    window = MainWindow()
+    window.show()
+    app.exec_()
---
+++ uitermometro.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui-termometro.ui'
+#
+# Created by: PyQt4 UI code generator 4.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(545, 452)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayout_3 = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setSpacing(10)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.graphicsView = PlotWidget(self.centralwidget)
+        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
+        self.verticalLayout_2.addWidget(self.graphicsView)
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setSpacing(15)
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.label_temperatura = QtGui.QLabel(self.centralwidget)
+        self.label_temperatura.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_temperatura.setAutoFillBackground(False)
+        self.label_temperatura.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_temperatura.setObjectName(_fromUtf8("label_temperatura"))
+        self.horizontalLayout_3.addWidget(self.label_temperatura)
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label_tempe_max = QtGui.QLabel(self.centralwidget)
+        self.label_tempe_max.setObjectName(_fromUtf8("label_tempe_max"))
+        self.horizontalLayout.addWidget(self.label_tempe_max)
+        self.spinBox_tempe_max = QtGui.QSpinBox(self.centralwidget)
+        self.spinBox_tempe_max.setObjectName(_fromUtf8("spinBox_tempe_max"))
+        self.horizontalLayout.addWidget(self.spinBox_tempe_max)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.checkBox_alarma = QtGui.QCheckBox(self.centralwidget)
+        self.checkBox_alarma.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.checkBox_alarma.setObjectName(_fromUtf8("checkBox_alarma"))
+        self.horizontalLayout_2.addWidget(self.checkBox_alarma)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout_3.addLayout(self.verticalLayout)
+        self.pushButton_Iniciar = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_Iniciar.setObjectName(_fromUtf8("pushButton_Iniciar"))
+        self.horizontalLayout_3.addWidget(self.pushButton_Iniciar)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 545, 25))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.toolBar = QtGui.QToolBar(MainWindow)
+        self.toolBar.setObjectName(_fromUtf8("toolBar"))
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.actionAjustes = QtGui.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/iconos/recursos/ajustes.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionAjustes.setIcon(icon)
+        self.actionAjustes.setObjectName(_fromUtf8("actionAjustes"))
+        self.actionInformacion = QtGui.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(_fromUtf8(":/iconos/recursos/icons8-acerca-de-100.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionInformacion.setIcon(icon1)
+        self.actionInformacion.setObjectName(_fromUtf8("actionInformacion"))
+        self.actionRefresh = QtGui.QAction(MainWindow)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(_fromUtf8(":/iconos/recursos/refrescar.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionRefresh.setIcon(icon2)
+        self.actionRefresh.setObjectName(_fromUtf8("actionRefresh"))
+        self.toolBar.addAction(self.actionAjustes)
+        self.toolBar.addAction(self.actionInformacion)
+        self.toolBar.addSeparator()
+        self.toolBar.addAction(self.actionRefresh)
+        self.label_tempe_max.setBuddy(self.spinBox_tempe_max)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.label_temperatura.setText(_translate("MainWindow", "0", None))
+        self.label_tempe_max.setText(_translate("MainWindow", "&Temperatura: ", None))
+        self.checkBox_alarma.setText(_translate("MainWindow", "&Alarma ", None))
+        self.pushButton_Iniciar.setText(_translate("MainWindow", "&Start", None))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar", None))
+        self.actionAjustes.setText(_translate("MainWindow", "Ajustes", None))
+        self.actionAjustes.setToolTip(_translate("MainWindow", "cambios de interfaz ", None))
+        self.actionInformacion.setText(_translate("MainWindow", "Informacion", None))
+        self.actionInformacion.setToolTip(_translate("MainWindow", "informacion acerca del programa", None))
+        self.actionRefresh.setText(_translate("MainWindow", "Refresh", None))
+        self.actionRefresh.setToolTip(_translate("MainWindow", "refrescar", None))
+
+from pyqtgraph import PlotWidget
+import recursosExternos_rc
